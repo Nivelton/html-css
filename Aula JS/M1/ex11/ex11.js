f7b0db971be9b656cd4ef9bed3d03cf5bbf14f58
Nivelton <niiveltoncph99@gmail.com>
2024-09-15 23:30:38 -0300

    js. aulas

---
+++ Aula JS/M1/ex11/ex11.js
@@ -0,0 +1,7 @@
+var vel = 61
+console.log(`a velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limmite de velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`
+)
